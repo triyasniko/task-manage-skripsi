4e684c94ceda83262abe9aac28d18e7aa6631916
triyasniko <triyasniko@gmail.com>
2023-01-04 21:06:27 +0700

    rename table name

---
+++ database/migrations/2022_10_05_032905_create_rel_kriterias_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRelKriteriasTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('Rel_Kriterias', function (Blueprint $table) {
+            $table->string('id_rel_kriterias', 10)->primary();
+            $table->string('id1');
+            $table->string('id2');
+            $table->string('nilai');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Rel_Kriterias');
+    }
+}
